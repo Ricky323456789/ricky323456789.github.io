8019ae8aa38126f7b82725de5e63b6ed8825b223
Ricky323456789 <guitarherorcb@yahoo.com>
2016-11-28 00:42:06 -0600

    Add files via upload

---
+++ ricky323456789.github.io/romas.js
@@ -0,0 +1,13 @@
+/*var slide =document.getElementById("menub");
+var pic=["url('box.jpg')","url('o.jpg')"];
+var x=0;
+function change(){
+slide.style.backgroundImage=pic[x];
+x++;
+if(x>2){
+x=0
+}
+var timer = setInterval(change(), 1000);
+}
+
+//slide.style.backgroundImage=pic[0];
